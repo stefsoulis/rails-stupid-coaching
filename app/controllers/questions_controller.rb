46ce9a0f5ba7f7d749ec9a6fd871351a025a41ac
Stefanos Soulis <stefsoulis@gmail.com>
2023-08-21 23:05:33 +0200

    routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,4 @@
 class QuestionsController < ApplicationController
   def ask
-    @questions = ['hello', 'what time is it']
-
-    search = params[:question]
-
-    if search
-      @questions = @questions.select do |question|
-        question == search.downcase
-      end
-    end
   end
 end
